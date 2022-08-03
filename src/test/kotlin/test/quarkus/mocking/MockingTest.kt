f24a30ed854e6fde43a4ccc2d71adb70c4dab0ef
Vadim Hleif <vadzim.hleif@gmail.com>
2022-08-03 11:43:15 +0400

    feat: example without Quarkus CDI

--- src/test/kotlin/test/quarkus/mocking/MockingTest.kt
+++ src/test/kotlin/test/quarkus/mocking/MockingTest.kt
@@ -5,12 +5,26 @@ import io.quarkus.test.junit.mockito.InjectMock
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
+import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
 import javax.inject.Inject
 
 
 @QuarkusTest
 internal class MockingTest {
+
+    private  val engineManuallyCreated: Engine = mock()
+    private val carManuallyCreated: Car = Car(engineManuallyCreated)
+
+    @Test
+    fun `both object are created without quarkus CDI -- it works`() {
+        whenever(engineManuallyCreated.launch(any())).thenReturn("not ready")
+        assertEquals(engineManuallyCreated.launch(1), "not ready")
+        logClassAndHash("from test (OK)", engineManuallyCreated)
+
+        assertEquals(carManuallyCreated.launch(1), "car is not ready")
+    }
+
 
     @Inject
     private lateinit var car: Car
